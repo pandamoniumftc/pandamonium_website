bd77c59c4fb461a6959ebd6e823c9473fffaeb29
jtk16 <Jack.t.kinney@gmail.com>
2023-02-03 17:24:02 -0800

    testing

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,32 @@ import TheWelcome from "../components/TheWelcome.vue";
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+    <header>
+        <link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
+        <h1><strong>We are team Pandamonium</strong></h1>
+    </header>
+    <nav>
+        <ul>
+        <li><router-link to="index.html">About</router-link></li>
+        <li><router-link to="team.html">Team</router-link></li>
+        <li><router-link to="outreach.html">Outreach</router-link></li>
+        </ul>
+    </nav>
+    <hr/>
+    <section id="about">
+        <h2>About Us</h2>
+        <p>Pandamonium Robotics is a First Tech Challenge team based in XYZ. We strive to inspire and motivate young people to become science and technology leaders by engaging them in exciting mentor-based programs that build STEM skills, that inspire innovation, and that foster well-rounded life capabilities.</p>
+    </section>
+    <section id="contact">
+        <h2>Contact Us</h2>
+        <form action="submit-form.php" method="post">
+        <label for="name">Name:</label>
+        <input type="text" id="name" name="name"><br>
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email"><br>
+        <label for="message">Message:</label>
+        <textarea id="message" name="message"></textarea><br>
+        <input type="submit" value="Send">
+        </form>
+    </section>
 </template>
