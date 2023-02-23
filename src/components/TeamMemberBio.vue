78fd08c4e747133d38c2a03898512ba41257d971
jtk16 <jack.t.kinney@gmai.com>
2023-02-23 10:43:17 -0800

    howdy

---
+++ src/components/TeamMemberBio.vue
@@ -0,0 +1,19 @@
+<script setup>
+defineProps({
+    imgPath: {
+        type: String,
+        required: true
+    },
+    msg: {
+        type: String,
+        required: true,
+    },
+});
+</script>
+
+<template>
+  <img src={{imgPath}}>
+  <p>{{msg}}</p>
+</template>
+
+
