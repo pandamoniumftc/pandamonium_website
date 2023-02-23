ce8bd1ff6afbec98edd3043e51e0903f26bd5cba
jtk16 <jack.t.kinney@gmai.com>
2023-02-23 09:52:15 -0800

    mac push

--- src/router/index.js
+++ src/router/index.js
@@ -17,6 +17,12 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import("../views/AboutView.vue"),
     },
+    {
+      path: "/team",
+      name: "team",
+
+      component: () => import("../views/TeamView.vue")
+    }
   ],
 });
 
