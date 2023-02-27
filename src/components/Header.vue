23b6e927db479dc4201ee3f401fedd20f0911eae
jtk16 <Jack.t.kinney@gmail.com>
2023-02-26 18:23:44 -0800

    css uwu

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -9,21 +9,28 @@
 
 <style scoped>
     #logo {
-        width: 30px;
-        height: 30px;
+        width: 60px;
+        height: 60px;
+        justify-items: left;
     }
 
     #first {
-        width: 30px;
-        height: 30px;
+        width: 60px;
+        height: 60px;
+        justify-items: right;
     }
 
     div {
         background-color: #231f20; /* light gray */
         padding: 20px;
         color: #f2f2f2;
-        text-align: center;
+        text-align: left;
     }
+
+    div h1 {
+        font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif ;
+        font-size: 6em;
+    }   
       
     #logo, #title, #first {
         display: inline-block;
