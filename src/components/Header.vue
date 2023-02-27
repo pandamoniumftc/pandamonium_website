3d00131512756ec1210f261cd83c279f5cfd86ea
jtk16 <Jack.t.kinney@gmail.com>
2023-02-26 17:52:59 -0800

    yeehaw

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,33 @@
 <template>
-    <img src="../assets/panda-logo-header.png"> 
+    <img src="../assets/panda-logo-header.png" id="logo"> 
     <h1>PANDAMONIUM</h1>
-    <img src="../assets/first-logo-header.png">
+    <img src="../assets/first-logo-header.png" id="first">
 </template>
+
+<style scoped>
+
+    img #logo {
+        width: 30px;
+        height: 30px;
+        align-items: center;
+    }
+
+    img first {
+        width: 30px;
+        height: 30px;
+        align-items: center;
+    }
+
+    template {
+        background-color: #333; /* light gray */
+        padding: 20px;
+        color: #f2f2f2;
+        text-align: center;
+    }
+      
+    template h1 {
+        margin: 0;
+        color: #f2f2f2; /* dark gray */
+        font-size: 2em;
+    }
+</style>
